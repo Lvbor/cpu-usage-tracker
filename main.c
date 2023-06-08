ce923141af40fc76b2574768dd69349e9e1d60ab
Lvbor <luborwitkowski123@gmail.com>
2023-06-08 22:04:43 +0200

    SIGTERM signal handling added. Got rid of all memory leaks (valgrind test).

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#define _XOPEN_SOURCE 700
 // Headers
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,27 +6,30 @@
 #include <string.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
 
 // Defines
 #define NUM_CORES 4
 #define MAX_BUFFER 256
-#define TRUE 1
 #define WATCHDOG_TIMEOUT 2
 
 // A structure that stores data for each core
-typedef struct {
+typedef struct
+{
     unsigned long user;    // Time used for user tasks
     unsigned long nice;    // Time used for "nice" tasks
     unsigned long system;  // Time used for system tasks
     unsigned long idle;    // Processor idle time
     unsigned long irq;     // Time used for servicing interrupts
     unsigned long softirq; // Time used for servicing software interrupts
-    unsigned long steal;   // Stolen time (virtualization) [OracleVM]
+    unsigned long steal;   // Stolen time (virtualization)
 } CPU_Data_t;
 
 // A structure that stores data for each thread
 #pragma pack(1) // Disable padding
-typedef struct {
+typedef struct
+{
     CPU_Data_t prev_data;
     CPU_Data_t curr_data;
     pthread_t reader_thread_id;
@@ -36,25 +40,42 @@ typedef struct {
 #pragma pack(4) // Reset to previous alignment rule (enable padding)
 
 // Logger thread data
-typedef struct {
+typedef struct
+{
     pthread_t logger_thread_id;
-    FILE* log_file;
+    FILE *log_file;
     pthread_mutex_t log_file_lock;
 } Logger_Data_t;
 
-static float printData[NUM_CORES]; // Shared data structure for CPU usage data
+static float printData[NUM_CORES];                                // Shared data structure for CPU usage data
 static pthread_mutex_t printDataLock = PTHREAD_MUTEX_INITIALIZER; // Definition of printDataLock (mutex)
+static volatile sig_atomic_t flagSigTerm = 0;                     // Flag which detects SIGTERM signal
+static Thread_Data_t threads[NUM_CORES];                          // Array of Thread_Data_t structures to store data for each thread.
+
+// Signal handler function for SIGTERM signal
+static void sigHandler(int signal)
+{
+    if (signal == SIGTERM)
+    {
+        printf("Received SIGTERM signal. Closing gracefully...\n");
+        flagSigTerm = 1;
+        sleep(2); // Wait 2 seconds to shutdown gracefully
+    }
+}
 
 // Reader thread
-static void *readerThread(void *arg) {
+static void *readerThread(void *arg)
+{
     Thread_Data_t *threadData = (Thread_Data_t *)arg;
     char buffer[MAX_BUFFER];
 
-    while (TRUE) {
+    while (!flagSigTerm)
+    {
         // Create the file pointer for each thread
         FILE *file = fopen("/proc/stat", "r"); // Opening /proc/stat (read)
         // Checks for NULL exception
-        if (file == NULL) {
+        if (file == NULL)
+        {
             perror("Failed to open /proc/stat");
             exit(EXIT_FAILURE);
         }
@@ -63,7 +84,8 @@ static void *readerThread(void *arg) {
         fseek(file, 0, SEEK_SET);
 
         // Read the overall CPU usage data
-        if (fgets(buffer, sizeof(buffer), file) == NULL) {
+        if (fgets(buffer, sizeof(buffer), file) == NULL)
+        {
             perror("Failed to read /proc/stat");
             exit(EXIT_FAILURE);
         }
@@ -76,7 +98,8 @@ static void *readerThread(void *arg) {
                    &(threadData->curr_data.idle),
                    &(threadData->curr_data.irq),
                    &(threadData->curr_data.softirq),
-                   &(threadData->curr_data.steal)) != 7) {
+                   &(threadData->curr_data.steal)) != 7)
+        {
             fprintf(stderr, "Failed to parse CPU data\n");
             exit(EXIT_FAILURE);
         }
@@ -94,12 +117,14 @@ static void *readerThread(void *arg) {
     return NULL;
 }
 
-static void *analyzerThread(void *arg) {
+static void *analyzerThread(void *arg)
+{
     Thread_Data_t *threadData = (Thread_Data_t *)arg;
 
     threadData->prev_data = threadData->curr_data; // Initialize prev_data
 
-    while (1) {
+    while (!flagSigTerm)
+    {
         // Getting the values needed to calculate CPU usage
         unsigned long prev_idle = threadData->prev_data.idle +
                                   threadData->prev_data.irq +
@@ -125,9 +150,12 @@ static void *analyzerThread(void *arg) {
 
         // Calculating the percentage of CPU usage
         float cpu_usage;
-        if (total_diff != 0) {
+        if (total_diff != 0)
+        {
             cpu_usage = (float)(total_diff - idle_diff) * 100.0f / (float)total_diff;
-        } else {
+        }
+        else
+        {
             cpu_usage = 0.0f;
         }
 
@@ -149,25 +177,37 @@ static void *analyzerThread(void *arg) {
     return NULL;
 }
 
-static void *printerThread() {
+static void *printerThread()
+{
 
-    while (1) {
+    while (!flagSigTerm)
+    {
         float total_cpu_usage = 0.0f;
         float average_cpu_usage = 0.0f;
+
         // Acquire the lock to safely access the shared printData structure
         pthread_mutex_lock(&printDataLock);
 
         // Calculate the total CPU usage
-        for (int i = 0; i < NUM_CORES; i++) {
+        for (int i = 0; i < NUM_CORES; i++)
+        {
             total_cpu_usage += printData[i];
         }
 
         // Calculate the average CPU usage
-        average_cpu_usage = total_cpu_usage / NUM_CORES;
+        if (NUM_CORES != 0)
+        {
+            average_cpu_usage = total_cpu_usage / NUM_CORES;
+        }
+        else
+        {
+            average_cpu_usage = 0.0f;
+        }
+
+        // Release the lock
+        pthread_mutex_unlock(&printDataLock);
 
         printf("Average CPU usage: %.2f%%\n", (double)average_cpu_usage);
-
-        pthread_mutex_unlock(&printDataLock);
 
         // Sleep for 1s
         sleep(1);
@@ -176,16 +216,21 @@ static void *printerThread() {
     return NULL;
 }
 
-static void *watchdogThread(void *arg) {
-    Thread_Data_t *threads = (Thread_Data_t *)arg;
+static void *watchdogThread()
+{
 
-    while (1) {
+    while (!flagSigTerm)
+    {
         sleep(WATCHDOG_TIMEOUT);
-        for (int i = 0; i < NUM_CORES; i++) {
-            if (!threads[i].is_alive) {
+        for (int i = 0; i < NUM_CORES; i++)
+        {
+            if (!threads[i].is_alive)
+            {
                 fprintf(stderr, "Thread %d did not report in time. Exiting...\n", i);
                 exit(EXIT_FAILURE);
-            } else {
+            }
+            else
+            {
                 threads[i].is_alive = false;
             }
         }
@@ -194,58 +239,76 @@ static void *watchdogThread(void *arg) {
     return NULL;
 }
 
-static void *loggerThread(void *arg) {
+static void *loggerThread(void *arg)
+{
     Logger_Data_t *loggerData = (Logger_Data_t *)arg;
     FILE *file;
 
-    while (1) {
+    while (!flagSigTerm)
+    {
         // Open the log file
         pthread_mutex_lock(&loggerData->log_file_lock); // Lock
         file = loggerData->log_file;
         pthread_mutex_unlock(&loggerData->log_file_lock); // Release
 
-        // Acquire the lock to safely access the shared printData structure
-        pthread_mutex_lock(&printDataLock);
+        if (file != NULL)
+        {
+            // Acquire the lock to safely access the shared printData structure
+            pthread_mutex_lock(&printDataLock);
 
-        // Write CPU usage data to the log file
-        for (int i = 0; i < NUM_CORES; i++) {
-            fprintf(file, "Thread %d CPU usage: %.2f%%\n", i, (double)printData[i]);
+            // Write CPU usage data to the log file
+            for (int i = 0; i < NUM_CORES; i++)
+            {
+                fprintf(file, "Thread %d CPU usage: %.2f%%\n", i, (double)printData[i]);
+            }
+
+            // Release the lock
+            pthread_mutex_unlock(&printDataLock);
         }
-
-        // Release the lock
-        pthread_mutex_unlock(&printDataLock);
 
         // Sleep for 1s
         sleep(1);
     }
-
     return NULL;
 }
 
-
-int main() {
-    Thread_Data_t threads[NUM_CORES];
+int main()
+{
     pthread_t printer_thread, watchdog_thread;
     Logger_Data_t loggerData;
+    struct sigaction action;
+    memset(&action, 0, sizeof(struct sigaction));
+#if defined(__clang__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdisabled-macro-expansion"
+/*
+The sa_handler in __sigaction_handler.sa_handler refers to a member of the __sigaction_handler
+union, not the sa_handler macro, so I decided to disable this warning for the clang compilier.
+*/
+#endif
+    action.sa_handler = sigHandler;
+#if defined(__clang__)
+#pragma GCC diagnostic pop
+#endif
+    sigaction(SIGTERM, &action, NULL);
+    printf("Process ID: %d\n", getpid());
     loggerData.log_file = fopen("log.txt", "w"); // Open the log file for writing
-    if (loggerData.log_file == NULL) {
+    if (loggerData.log_file == NULL)
+    {
         perror("Failed to open log file");
         exit(EXIT_FAILURE);
     }
     pthread_mutex_init(&loggerData.log_file_lock, NULL); // Initialize the log file lock
 
-    // Set the log file pointer in the logger thread data
-    pthread_mutex_lock(&loggerData.log_file_lock);
-    loggerData.log_file = fopen("log.txt", "w"); // Open the log file for writing
-    pthread_mutex_unlock(&loggerData.log_file_lock);
-
-    if (loggerData.log_file == NULL) {
+    if (loggerData.log_file == NULL)
+    {
         perror("Failed to open log file");
         exit(EXIT_FAILURE);
     }
 
     // Reader and analyzer thread initialization
-    for (int i = 0; i < NUM_CORES; i++) {
+    for (int i = 0; i < NUM_CORES; i++)
+    {
         threads[i].thread_index = i;
         threads[i].is_alive = false;
         pthread_create(&threads[i].reader_thread_id, NULL, readerThread, &threads[i]);
@@ -260,20 +323,29 @@ int main() {
 
     // Logger thread initialization
     pthread_create(&loggerData.logger_thread_id, NULL, loggerThread, &loggerData);
+    
+    while (!flagSigTerm)
+    {
+        for (int i = 0; i < NUM_CORES; i++)
+        {
+            pthread_join(threads[i].reader_thread_id, NULL);
+            pthread_join(threads[i].analyzer_thread_id, NULL);
+        }
 
-    for (int i = 0; i < NUM_CORES; i++) {
-        pthread_join(threads[i].reader_thread_id, NULL);
-        pthread_join(threads[i].analyzer_thread_id, NULL);
+        pthread_join(printer_thread, NULL);
+        pthread_join(watchdog_thread, NULL);
+        pthread_join(loggerData.logger_thread_id, NULL);
     }
-
-    pthread_join(printer_thread, NULL);
-    pthread_join(watchdog_thread, NULL);
-    pthread_join(loggerData.logger_thread_id, NULL);
 
     // Close the log file
     pthread_mutex_lock(&loggerData.log_file_lock);
-    fclose(loggerData.log_file);
+    if (fclose(loggerData.log_file) != 0)
+    {
+        perror("Failed to close log file");
+        exit(EXIT_FAILURE);
+    }
     pthread_mutex_unlock(&loggerData.log_file_lock);
+    printf("Cleaning successful\n");
 
     return 0;
 }
